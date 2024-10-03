c6f1d36bf0541056f06b3c26f22e42f7fe456a01
Gaurav Singh <gauravsingh1892000@gmail.com>
2024-10-03 20:53:34 +0530

    Url Shortener

---
+++ urlshortener/src/test/java/backend/net/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.net.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
